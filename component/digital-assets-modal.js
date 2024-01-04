96293c5d6cd66693a7e578f87e5c1bec62591f54
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2024-01-05 02:56:59 +0800

    v1.0.11

--- component/digital-assets-modal.js
+++ component/digital-assets-modal.js
@@ -9,6 +9,45 @@ const typeName = {
   },
 };
 
+function sucsessModal() {
+  return `
+        <div class="content-56">
+          <img
+            src="images/Digital-coins.png"
+            loading="lazy"
+            sizes="(max-width: 479px) 80vw, 351.9921875px"
+            srcset="
+              images/Digital-coins-p-500.png   500w,
+              images/Digital-coins-p-800.png   800w,
+              images/Digital-coins-p-1080.png 1080w,
+              images/Digital-coins.png        1417w
+            "
+            alt=""
+            class="image-12"
+          />
+        </div>
+        <div class="modal-header-3">
+          <div class="content-57">
+            <div class="text-and-supporting-text-28">
+              <div class="text-lg-semibold-6">
+                Digital Asset <br />successfully registered
+              </div>
+              <div class="text-sm-regular-12">
+                The account has been registered <br />and ready for your
+                wasiat/will
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-actions-3">
+          <div class="content-58">
+            <a class="button-30" type="button" data-dismiss="modal">
+              <div class="text-md-semibold-9">Finish</div>
+            </a>
+          </div>
+        </div>`;
+}
+
 function digitalAssetsForm(type) {
   if (!(type in typeName)) {
     console.error(`Invalid type: ${type}`);
@@ -16,58 +55,50 @@ function digitalAssetsForm(type) {
   }
 
   return `
-  <div
-  class="modal fade"
-  id="${typeName[type].key}-digital-assets-modal"
->
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content rounded-3">
-      <div class="modal-header">
-        <button
-          type="button"
-          class="close"
-          data-dismiss="modal"
-        >
-          &times;
-        </button>
-      </div>
-
+  <div class="modal fade" id="${typeName[type].key}-digital-assets-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
       <div class="modal-body">
-        <div class="modal-header-2">
-          <div class="content-32">
-            <div class="smpl-icon-featured-outline-large">
-              <div class="uui-icon-1x1-xsmall-2 w-embed">
-                <svg
-                  width="24"
-                  height="24"
-                  viewbox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M6 21H9M15 21H18M17.5 6.5V14.5M3 6.2L3 14.8C3 15.9201 3 16.4802 3.21799 16.908C3.40973 17.2843 3.71569 17.5903 4.09202 17.782C4.51984 18 5.07989 18 6.2 18L17.8 18C18.9201 18 19.4802 18 19.908 17.782C20.2843 17.5903 20.5903 17.2843 20.782 16.908C21 16.4802 21 15.9201 21 14.8V6.2C21 5.0799 21 4.51984 20.782 4.09202C20.5903 3.7157 20.2843 3.40974 19.908 3.21799C19.4802 3 18.9201 3 17.8 3L6.2 3C5.0799 3 4.51984 3 4.09202 3.21799C3.7157 3.40973 3.40973 3.71569 3.21799 4.09202C3 4.51984 3 5.07989 3 6.2ZM11.5 10.5C11.5 11.8807 10.3807 13 9 13C7.61929 13 6.5 11.8807 6.5 10.5C6.5 9.11929 7.61929 8 9 8C10.3807 8 11.5 9.11929 11.5 10.5Z"
-                    stroke="#3118D3"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-            <div class="text-and-supporting-text-18">
-              <div class="text-lg-semibold-4">
-                Account Details
-              </div>
-              <div class="text-sm-regular-6">
-                Ensure no assets will be left behind for
-                your loved ones.
-              </div>
-            </div>
-          </div>
-          <div class="padding-bottom-3"></div>
+        <div>
+          <button type="button" class="close" data-dismiss="modal">
+            &times;
+          </button>
         </div>
-        <div class="spacer-30"></div>
-        <div id="digital-assets-form-container">
+        <div class="hidden" id="${
+          typeName[type].key
+        }-success-body-container">${sucsessModal()}</div>
+        <div id="${typeName[type].key}-form-body-container">
+          <div class="modal-header-2">
+            <div class="content-32">
+              <div class="smpl-icon-featured-outline-large">
+                <div class="uui-icon-1x1-xsmall-2 w-embed">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewbox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6 21H9M15 21H18M17.5 6.5V14.5M3 6.2L3 14.8C3 15.9201 3 16.4802 3.21799 16.908C3.40973 17.2843 3.71569 17.5903 4.09202 17.782C4.51984 18 5.07989 18 6.2 18L17.8 18C18.9201 18 19.4802 18 19.908 17.782C20.2843 17.5903 20.5903 17.2843 20.782 16.908C21 16.4802 21 15.9201 21 14.8V6.2C21 5.0799 21 4.51984 20.782 4.09202C20.5903 3.7157 20.2843 3.40974 19.908 3.21799C19.4802 3 18.9201 3 17.8 3L6.2 3C5.0799 3 4.51984 3 4.09202 3.21799C3.7157 3.40973 3.40973 3.71569 3.21799 4.09202C3 4.51984 3 5.07989 3 6.2ZM11.5 10.5C11.5 11.8807 10.3807 13 9 13C7.61929 13 6.5 11.8807 6.5 10.5C6.5 9.11929 7.61929 8 9 8C10.3807 8 11.5 9.11929 11.5 10.5Z"
+                      stroke="#3118D3"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+              <div class="text-and-supporting-text-18">
+                <div class="text-lg-semibold-4">Account Details</div>
+                <div class="text-sm-regular-6">
+                  Ensure no assets will be left behind for your loved ones.
+                </div>
+              </div>
+            </div>
+            <div class="padding-bottom-3"></div>
+          </div>
+          <div class="spacer-30"></div>
           <form
             id="${typeName[type].key}-digital-assets-form"
             name="wf-form-Digital-Account-form"
@@ -80,12 +111,8 @@ function digitalAssetsForm(type) {
               class="form-content-2"
             >
               <div class="form-field-wrapper">
-                <label
-                  for="Username-4"
-                  class="field-label"
-                  >Username<span class="text-span-9"
-                    >*</span
-                  ></label
+                <label for="Username-4" class="field-label"
+                  >Username<span class="text-span-9">*</span></label
                 ><input
                   type="text"
                   class="form_input w-input"
@@ -99,9 +126,7 @@ function digitalAssetsForm(type) {
               </div>
               <div class="form-field-wrapper">
                 <label for="email-4" class="field-label"
-                  >Email<span class="text-span-10"
-                    >*</span
-                  ></label
+                  >Email<span class="text-span-10">*</span></label
                 ><input
                   type="email"
                   class="form_input w-input"
@@ -119,13 +144,8 @@ function digitalAssetsForm(type) {
               class="form-content-2"
             >
               <div class="form-field-wrapper">
-                <label
-                  for="Service-Platform-5"
-                  class="field-label"
-                  >Service Platform<span
-                    class="text-span-8"
-                    >*</span
-                  ></label
+                <label for="Service-Platform-5" class="field-label"
+                  >Service Platform<span class="text-span-8">*</span></label
                 ><select
                   id="select-${typeName[type].key}-service-platform"
                   name="Service-Platform-4"
@@ -137,12 +157,8 @@ function digitalAssetsForm(type) {
                 </select>
               </div>
               <div class="form-field-wrapper">
-                <label
-                  for="Declared-value-4"
-                  class="field-label"
-                  >Type<span class="text-span-7"
-                    >*</span
-                  ></label
+                <label for="Declared-value-4" class="field-label"
+                  >Type<span class="text-span-7">*</span></label
                 ><select
                   id="select-${typeName[type].key}-type"
                   name="Declared-value-3"
@@ -158,12 +174,8 @@ function digitalAssetsForm(type) {
               class="form-content-2"
             >
               <div class="form-field-wrapper">
-                <label
-                  for="Declared-value-4"
-                  class="field-label"
-                  >Frequency<span class="text-span-7"
-                    >*</span
-                  ></label
+                <label for="Declared-value-4" class="field-label"
+                  >Frequency<span class="text-span-7">*</span></label
                 ><select
                   id="select-${typeName[type].key}-frequency"
                   name="Declared-value-3"
@@ -175,13 +187,8 @@ function digitalAssetsForm(type) {
                 </select>
               </div>
               <div class="form-field-wrapper">
-                <label
-                  for="Declared-value-4"
-                  class="field-label"
-                  >Declared Value (MYR)<span
-                    class="text-span-6"
-                    >*</span
-                  ></label
+                <label for="Declared-value-4" class="field-label"
+                  >Declared Value (MYR)<span class="text-span-6">*</span></label
                 ><select
                   id="select-${typeName[type].key}-declared-value"
                   name="Declared-value-3"
@@ -195,8 +202,7 @@ function digitalAssetsForm(type) {
             </div>
             <div class="form-field-wrapper">
               <label for="field-3" class="field-label"
-                >Instructions After Death<span
-                  class="text-span-11"
+                >Instructions After Death<span class="text-span-11"
                   >*</span
                 ></label
               ><select
@@ -211,9 +217,7 @@ function digitalAssetsForm(type) {
             </div>
             <div class="form-field-wrapper">
               <label for="field-3" class="field-label"
-                >Beneficiary<span class="text-span-11"
-                  >*</span
-                ></label
+                >Beneficiary<span class="text-span-11">*</span></label
               ><select
                 id="select-${typeName[type].key}-beneficiary"
                 name="field-2"
@@ -225,9 +229,7 @@ function digitalAssetsForm(type) {
               </select>
             </div>
             <div class="form-field-wrapper">
-              <label
-                for="Contact03-message-2"
-                class="field-label"
+              <label for="Contact03-message-2" class="field-label"
                 >Remarks</label
               ><textarea
                 id="input-${typeName[type].key}-remarks"
@@ -255,22 +257,18 @@ function digitalAssetsForm(type) {
                   for="Contact-03-checkbox"
                   class="text-size-small w-form-label"
                   >You agree to our friendly
-                  <a
-                    href="legal.html"
-                    class="text-style-link"
-                    >privacy policy</a
+                  <a href="legal.html" class="text-style-link">privacy policy</a
                   >.</span
                 >
               </label>
             </div>
-
-            <input
+            <button
               type="submit"
-              value="${typeName[type].buttonTitle}"
-              data-wait="Please wait..."
-              id="w-node-_4ac9bd4b-46c5-1204-d604-d2bf44b33a31-e2e93042"
-              class="button w-button"
-            />
+              class="w-button button"
+              id="${typeName[type].key}-digital-assets-btn"
+            >
+            ${typeName[type].buttonTitle}
+            </button>
           </form>
         </div>
       </div>
