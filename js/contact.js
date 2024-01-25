2b9f6df00e2456e0f10a151e6551d7914ab043d7
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2024-01-25 20:01:27 +0800

    v1.0.58

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,10 @@
-document.getElementById('footer-container').innerHTML = footer();
-newsletterFormAddAPI();
-document.getElementById('nav-bar-container').innerHTML = navBar();
-navBarAuthUpdate();
-
 const inputElements = {
   add_contact_us_form: {
     name: document.getElementById('input-contact-us-name'),
     email: document.getElementById('input-contact-us-email'),
     message: document.getElementById('input-contact-us-message'),
   },
-  view_contact: {
-    email: document.getElementById('view-contact-email'),
-    phone_no: document.getElementById('view-contact-phone-no'),
-    address: document.getElementById('view-contact-address'),
-  },
 };
-
-function updateViewContactInfo(info) {
-  for (const key in inputElements.view_contact) {
-    if (info.hasOwnProperty(key)) {
-      inputElements.view_contact[key].innerHTML = info[key];
-    }
-  }
-}
-
-inputElements.view_contact.email.addEventListener(
-  'click',
-  async function (event) {
-    openEmailApp(companyInfo.email);
-  }
-);
 
 document
   .getElementById('add-contact-us-form')
@@ -51,20 +26,17 @@ document
 
     if (error) {
       console.error('Error', error.message);
-      showToast('alert-toast-container', error.message, 'danger');
-    } else {
-      processForm(inputElements.add_contact_us_form, true);
-      showToast(
-        'alert-toast-container',
-        'Thank you for reaching out! your message has been successfully submitted!',
-        'success'
-      );
+      handleFormResult({ error, useBtn, defaultBtnText });
+      return;
     }
 
-    useBtn.disabled = false;
-    useBtn.innerHTML = defaultBtnText;
-  });
+    processForm(inputElements.add_contact_us_form, true);
 
-$(document).ready(function () {
-  updateViewContactInfo(companyInfo);
-});
+    handleFormResult({
+      error,
+      useBtn,
+      defaultBtnText,
+      successText:
+        'Thank you for reaching out! your message has been successfully submitted!',
+    });
+  });
